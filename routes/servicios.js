a0ebf1799b3d6c03691def78add2a3340d63bf6e
gcosta87 <gonzalogcosta@yahoo.com.ar>
2013-03-01 19:20:12 -0300

    Cambios Menores

--- routes/servicios.js
+++ routes/servicios.js
@@ -129,6 +129,8 @@ exports.acciones=function(req,res){
 		formato.push(yahooFinanzas.formato.ultimaCotizacion)
 		formato.push(yahooFinanzas.formato.variacion)
 		formato.push(yahooFinanzas.formato.variacionEnPorcentaje)
+		formato.push(yahooFinanzas.formato.valorDeApertura)
+		formato.push(yahooFinanzas.formato.valorCierreAnterior)
 		formato.push(yahooFinanzas.formato.fecha)
 		formato.push(yahooFinanzas.formato.hora)
 				
@@ -136,15 +138,26 @@ exports.acciones=function(req,res){
 			//Genero la respuesta
 			respuesta=respuesta.replace(/["%]/g,'')
 			campos=respuesta.split(',');
+			
+			//cambio el formato de la fecha: mm/dd/aaaa -> dd/mm/aaaa
+			regexFecha=/([0-9]{1,2})[/]([0-9]{1,2})[/]([0-9]{2,4})/
+			camposFecha= regexFecha.exec(campos[7]);
+
 			accion={
-				simbolo:				campos[0],
+				simbolo:				campos[0].replace('.BA',''),
 				nombre:					campos[1],
 				descripcion:			accion.nombre,
 				ultimaCotizacion:		funcionesDeServicios.convertirEnFloat(campos[2]),
 				variacion:				funcionesDeServicios.convertirEnFloat(campos[3]),
 				variacionEnPorcentaje:	funcionesDeServicios.convertirEnFloat(campos[4]),
-				fecha:					campos[5],
-				hora:					campos[6]
+				valorDeApertura:		funcionesDeServicios.convertirEnFloat(campos[5]),
+				valorCierreAnterior:	funcionesDeServicios.convertirEnFloat(campos[6]),
+				fecha:					camposFecha[2]+'/'+camposFecha[1]+'/'+camposFecha[3],	//Original: campos[5],
+				hora:					campos[8].trim(),
+				fuente:{
+					nombre:		'Yahoo! Finanzas Argentina',
+					url:		'http://ar.finanzas.yahoo.com/'
+				}
 			}
 			
 			//Envio la respuesta al usr
@@ -153,7 +166,7 @@ exports.acciones=function(req,res){
 		});		
 	}
 	else{
-		res.send('404','No existe la Acción solicitada')
+		res.send('404','No existe la Acción solicitada: '+req.params.simbolo)
 	}
 }
 
@@ -166,19 +179,19 @@ exports.accionesColeccion=function(req,res){
 		descripcion: "Consulte las acciones del Merval",
 		documentacion: "/api/acciones/",
 		acciones:{
-			'alua.ba':	{simbolo: 'ALUA.BA', nombre: 'Aluar Aluminio Argentino S.A.I.C', url: '/api/acciones/alua.ba'},
-			'apbr.ba':	{simbolo: 'APBR.BA', nombre: 'Petroleo Brasileiro SA Petrobras', url: '/api/acciones/apbr.ba'},
-			'bma.ba':	{simbolo: 'BMA.BA', nombre: 'Macro Bank, Inc.', url: '/api/acciones/bma.ba'},
-			'come.ba':	{simbolo: 'COME.BA', nombre: 'Sociedad Comercial del Plata SA', url: '/api/acciones/come.ba'},
-			'edn.ba':	{simbolo: 'EDN.BA', nombre: 'EMP.DIST.Y COM.NORTE', url: '/api/acciones/edn.ba'},
-			'erar.ba':	{simbolo: 'ERAR.BA', nombre: 'Siderar S.A.I.C.', url: '/api/acciones/erar.ba'},
-			'fran.ba':	{simbolo: 'FRAN.BA', nombre: 'Bbva Banco Frances,S.A.', url: '/api/acciones/fran.ba'},
-			'ggal.ba':	{simbolo: 'GGAL.BA', nombre: 'Grupo Financiero Galicia SA', url: '/api/acciones/ggal.ba'},
-			'pamp.ba':	{simbolo: 'PAMP.BA', nombre: 'Pampa Energia SA', url: '/api/acciones/pamp.ba'},
-			'pesa.ba':	{simbolo: 'PESA.BA', nombre: 'Petrobras Argentina SA', url: '/api/acciones/pesa.ba'},
-			'teco2.ba':	{simbolo: 'TECO2.BA', nombre: 'Telecom Argentina SA', url: '/api/acciones/teco2.ba'},
-			'ts.ba':	{simbolo: 'TS.BA', nombre: 'Tenaris SA', url: '/api/acciones/ts.ba'},
-			'ypfd.ba':	{simbolo: 'YPFD.BA', nombre: 'YPF Sociedad Anonima', url: '/api/acciones/ypfd.ba'}
+			'alua.ba':	{simbolo: 'ALUA', nombre: 'Aluar Aluminio Argentino S.A.I.C', url: '/api/acciones/alua.ba'},
+			'apbr.ba':	{simbolo: 'APBR', nombre: 'Petroleo Brasileiro SA Petrobras', url: '/api/acciones/apbr.ba'},
+			'bma.ba':	{simbolo: 'BMA', nombre: 'Macro Bank, Inc.', url: '/api/acciones/bma.ba'},
+			'come.ba':	{simbolo: 'COME', nombre: 'Sociedad Comercial del Plata SA', url: '/api/acciones/come.ba'},
+			'edn.ba':	{simbolo: 'EDN', nombre: 'EMP.DIST.Y COM.NORTE', url: '/api/acciones/edn.ba'},
+			'erar.ba':	{simbolo: 'ERAR', nombre: 'Siderar S.A.I.C.', url: '/api/acciones/erar.ba'},
+			'fran.ba':	{simbolo: 'FRAN', nombre: 'Bbva Banco Frances,S.A.', url: '/api/acciones/fran.ba'},
+			'ggal.ba':	{simbolo: 'GGAL', nombre: 'Grupo Financiero Galicia SA', url: '/api/acciones/ggal.ba'},
+			'pamp.ba':	{simbolo: 'PAMP', nombre: 'Pampa Energia SA', url: '/api/acciones/pamp.ba'},
+			'pesa.ba':	{simbolo: 'PESA', nombre: 'Petrobras Argentina SA', url: '/api/acciones/pesa.ba'},
+			'teco2.ba':{simbolo: 'TECO2', nombre: 'Telecom Argentina SA', url: '/api/acciones/teco2.ba'},
+			'ts.ba':	{simbolo: 'TS', nombre: 'Tenaris SA', url: '/api/acciones/ts.ba'},
+			'ypfd.ba':	{simbolo: 'YPFD', nombre: 'YPF Sociedad Anonima', url: '/api/acciones/ypfd.ba'}
 		}
 	}
 
