e75049816fe67481a7f06ed68d9cdae89b49f73e
gcosta87 <gonzalogcosta@yahoo.com.ar>
2013-02-28 21:53:34 -0300

    Coleccion inicial de divisas y acciones

--- routes/servicios.js
+++ routes/servicios.js
@@ -31,6 +31,25 @@ tipoDeDivisas={
 	'dolarMayorista':	{elementoHTML:'.columna3',nombre:'Dolar Mayorista (Bancos)'}
 }
 
+tipoDeAcciones={
+	'alua.ba':	{simbolo: 'ALUA.BA', nombre: 'Aluar Aluminio Argentino S.A.I.C'},
+	'apbr.ba':	{simbolo: 'APBR.BA', nombre: 'Petroleo Brasileiro SA Petrobras'},
+	'bma.ba':	{simbolo: 'BMA.BA', nombre: 'Macro Bank, Inc.'},
+	'come.ba':	{simbolo: 'COME.BA', nombre: 'Sociedad Comercial del Plata SA'},
+	'edn.ba':	{simbolo: 'EDN.BA', nombre: 'EMP.DIST.Y COM.NORTE'},
+	'erar.ba':	{simbolo: 'ERAR.BA', nombre: 'Siderar S.A.I.C.'},
+	'fran.ba':	{simbolo: 'FRAN.BA', nombre: 'Bbva Banco Frances,S.A.'},
+	'ggal.ba':	{simbolo: 'GGAL.BA', nombre: 'Grupo Financiero Galicia SA'},
+	'pamp.ba':	{simbolo: 'PAMP.BA', nombre: 'Pampa Energia SA'},
+	'pesa.ba':	{simbolo: 'PESA.BA', nombre: 'Petrobras Argentina SA'},
+	'teco2.ba':	{simbolo: 'TECO2.BA', nombre: 'Telecom Argentina SA'},
+	'ts.ba':	{simbolo: 'TS.BA', nombre: 'Tenaris SA'},
+	'ypfd.ba':	{simbolo: 'YPFD.BA', nombre: 'YPF Sociedad Anonima'}	
+}
+
+
+
+
 //	FUNCIONES PRIVADAS
 //
 
@@ -38,8 +57,9 @@ tipoDeDivisas={
 //	FUNCIONES PUBLICAS
 //
 
-//Implementación del Servicios de Divisas: Dolar, Dolar Blue...
-//Estado borrador
+// nombre:		divisas
+// descripción:	Devuelve el valor de los diversas divisas: Dolar Ofical,Dolar Blue/Informal, Dolar Mayorista, Euro, Real,etc.... Fuente Ambito Financiero. Falta
+// estado:		Borrador: Solo soporta los distintos Dolares, faltan las restantes divisas.
 exports.divisas=function(req,res){
 	divisa=tipoDeDivisas[req.params.divisa];
 	
@@ -75,4 +95,60 @@ exports.divisas=function(req,res){
 	else{
 		res.send('404','No existe la divisa solicitada: '+req.params.divisa+'<br/><br/>Solo puede consultar:<ul><li><a href="/api/divisas/dolar">dolar</a><li><a href="/api/divisas/dolarBlue">dolarBlue</a><li><a href="/api/divisas/dolarMayorista">dolarMayorista</a></ul>')
 	}
+}
+
+// nombre:		divisasColeccion
+// descripción:	Retorna la coleccion de divisas (actualmente) disponibles para consultar
+// estado:		Borrador
+exports.divisasColeccion=function(req,res){
+	//Objeto respuesta
+	respuesta={
+		descripcion: "Consulte las principales divisas",
+		documentacion: "/api/divisas/",
+		divisas:{
+			dolar:			{nombre:tipoDeDivisas.dolar.nombre, url:'/api/divisas/dolar'},
+			dolarBlue:		{nombre:tipoDeDivisas.dolarBlue.nombre, url:'/api/divisas/dolarBlue'},
+			dolarMayorista:	{nombre:tipoDeDivisas.dolarMayorista.nombre, url:'/api/divisas/dolarMayorista'}
+		}
+	}
+
+	//Genero la respuesta segun la cabercera HTTP Acept del cliente: Texto plano, HTML...
+	funcionesDeServicios.respuestaDinamica(req,res,respuesta);
+}
+
+
+// nombre:		acciones
+// descripción:	Devuelve la cotización de las acciones de la empresa solicitada. Fuente Yahoo Finanzas.
+// estado:		Borrador
+exports.acciones=function(req,res){
+	
+}
+
+// nombre:		accionesColeccion
+// descripción:	Retorna la coleccion de acciones del merval que (actualmente) están disponibles.
+// estado:		Borrador
+exports.accionesColeccion=function(req,res){
+	//Objeto respuesta
+	respuesta={
+		descripcion: "Consulte las acciones del Merval",
+		documentacion: "/api/acciones/",
+		acciones:{
+			'alua.ba':	{simbolo: 'ALUA.BA', nombre: 'Aluar Aluminio Argentino S.A.I.C', url: '/api/acciones/alua.ba'},
+			'apbr.ba':	{simbolo: 'APBR.BA', nombre: 'Petroleo Brasileiro SA Petrobras', url: '/api/acciones/apbr.ba'},
+			'bma.ba':	{simbolo: 'BMA.BA', nombre: 'Macro Bank, Inc.', url: '/api/acciones/bma.ba'},
+			'come.ba':	{simbolo: 'COME.BA', nombre: 'Sociedad Comercial del Plata SA', url: '/api/acciones/come.ba'},
+			'edn.ba':	{simbolo: 'EDN.BA', nombre: 'EMP.DIST.Y COM.NORTE', url: '/api/acciones/edn.ba'},
+			'erar.ba':	{simbolo: 'ERAR.BA', nombre: 'Siderar S.A.I.C.', url: '/api/acciones/erar.ba'},
+			'fran.ba':	{simbolo: 'FRAN.BA', nombre: 'Bbva Banco Frances,S.A.', url: '/api/acciones/fran.ba'},
+			'ggal.ba':	{simbolo: 'GGAL.BA', nombre: 'Grupo Financiero Galicia SA', url: '/api/acciones/ggal.ba'},
+			'pamp.ba':	{simbolo: 'PAMP.BA', nombre: 'Pampa Energia SA', url: '/api/acciones/pamp.ba'},
+			'pesa.ba':	{simbolo: 'PESA.BA', nombre: 'Petrobras Argentina SA', url: '/api/acciones/pesa.ba'},
+			'teco2.ba':	{simbolo: 'TECO2.BA', nombre: 'Telecom Argentina SA', url: '/api/acciones/teco2.ba'},
+			'ts.ba':	{simbolo: 'TS.BA', nombre: 'Tenaris SA', url: '/api/acciones/ts.ba'},
+			'ypfd.ba':	{simbolo: 'YPFD.BA', nombre: 'YPF Sociedad Anonima', url: '/api/acciones/ypfd.ba'}
+		}
+	}
+
+
+	funcionesDeServicios.respuestaDinamica(req,res,respuesta);
 }
